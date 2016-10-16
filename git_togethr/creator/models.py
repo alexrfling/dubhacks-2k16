4d31edd9343888d32e12727ee702f14428583bcd
kev1234 <kev1234@cs.washington.edu>
2016-10-15 21:16:32 -0700

    created project class'

--- git_togethr/creator/models.py
+++ git_togethr/creator/models.py
@@ -7,10 +7,16 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
     bio = models.TextField()
-    area = models.IntegerField();
+    area = models.IntegerField()
     interest = models.CharField()
     project = models.ForeignKey('Project')
-    skills = models.CharField();
+    skills = models.CharField()
 
     def __str__(self):
         return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField()
+    creator = models.ForeignKey('User')
+    contributors = 
